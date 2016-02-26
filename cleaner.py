2077b830084e579e5016a4a107ab23f8ca5fad38
Vale <valetolpegin@gmail.com>
2016-02-25 20:54:16 -0500

    Add recipe tally

--- cleaner.py
+++ cleaner.py
@@ -40,16 +40,30 @@ class Cleaner():
         """
         Cleans the main haikuports directory & all its subfolders
         """
+        total_recipes = self.tally_recipes(directory_to_clean)
         recipe_index = 0
         for root, dirs, files in os.walk(directory_to_clean):
             path = root.split('/')
             print (len(path) - 1) *'---' , os.path.basename(root)
             for test_file in files:
                 if test_file.endswith(".recipe"):
-                    print len(path)*'---', test_file
+                    recipe_index += 1
+                    print len(path)*'---', test_file, ' ', recipe_index, '/', total_recipes
                     current_recipe_fixer = RecipeFixer(root, test_file, self.logFile)
                     current_recipe_fixer.clean()
-                    recipe_index += 1
 
         # Printing out the total recipe count
         print("Cleaned " + str(recipe_index) + " recipes")
+
+    def tally_recipes(self, base_directory):
+        """
+        Returns the total number of recipes located within the directory
+        base_directory
+        """
+        total_recipes = 0
+        for root, dirs, files in os.walk(base_directory):
+            for test_file in files:
+                if test_file.endswith(".recipe"):
+                    total_recipes += 1
+
+        return total_recipes
